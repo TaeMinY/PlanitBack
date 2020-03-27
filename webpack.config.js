293545ba1e1597a744aa87e95bc9d6d470b58161
UniTaeMin <onnu0802@naver.com>
2020-03-27 22:53:46 +0900

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,24 @@
-const path = require('path');
+const path = require("path")
 
 module.exports = {
-    entry: './src/app.ts',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    devtool: 'source-map',
-    target: 'node',
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    output: {
-        filename: 'app.js',
-        path: path.resolve(__dirname, 'build')
-    },
-	node: {
-		__dirname: false
-	}
-};
+  mode: "development",
+  entry: "./src/app.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devtool: "source-map",
+  target: "node",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "build")
+  }
+}
