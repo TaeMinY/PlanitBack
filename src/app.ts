7ca5407bbebe9cf7254463aa69af80a8012764ab
TaeMinY <onnu0802@naver.com>
2022-09-12 23:49:41 +0900

    Feat: Port Change

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import * as express from 'express';
-import * as compression from 'compression';
-import * as helmet from 'helmet';
-import * as morgan from 'morgan';
-import * as cors from 'cors';
+import * as express from "express";
+import * as compression from "compression";
+import * as helmet from "helmet";
+import * as morgan from "morgan";
+import * as cors from "cors";
 import Router from "./Routers/index";
 // import socket from "./Module/socket";
 const app: express.Application = express();
-const connectDB = require('./Module/connectDB');
-const PORT = process.env.PORT || 4000;
+const connectDB = require("./Module/connectDB");
+const PORT = process.env.PORT || 5002;
 const http = require("http");
 const server = http.createServer(app);
 connectDB();
-require('dotenv').config();
+require("dotenv").config();
 app.use(morgan("dev"));
 app.use(compression());
 app.use(helmet());
@@ -19,13 +19,13 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ limit: "30mb" }));
 app.use(express.static("public"));
-app.use(express.static('upload'));
+app.use(express.static("upload"));
 app.use("/", Router);
 app.get("/", (req: express.Request, res: express.Response) => {
-	res.send("Hello World!!!!");
+  res.send("Hello World!!!!");
 });
 server.listen(PORT, () => {
-    console.log(`http://localhost:${PORT} OnOn`);
+  console.log(`http://localhost:${PORT} OnOn`);
 });
 // const io = require('socket.io')(server);
 // socket(io)
